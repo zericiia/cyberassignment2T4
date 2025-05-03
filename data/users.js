56bfd15996bfbaba82e9cb452f377b55620b9d99
karim brahimi <karimbrahimi102@gmail.com>
2025-05-03 22:30:04 +0100

    added users

---
+++ data/users.js
@@ -0,0 +1,5 @@
+module.exports = [
+    { username: "admin", password: "admin123", role: "admin" },
+    { username: "wiener", password: "peter", role: "user" },
+    { username: "carlos", password: "montoya", role: "user" },
+];
